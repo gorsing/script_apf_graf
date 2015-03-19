fd23dd8956644d8f71535872d55ba1b8eabdaefc
Gorsing <gorsing@meta.ua>
2015-03-20 01:45:06 +0200

    rev 7

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import os
 
 def print_dir(path, prefix=u''):
 	print u'{}(Dir){}'.format(prefix, os.path.basename(path))
-	file_1.write('}'+'\n'+u'subgraph '+'"'+str(os.path.basename(path))+'"'+u'{'+'\n')
+	file_1.write('}'+'\n'+u'subgraph '+'"'+str(os.path.basename(path))+'"'+u'{'+'\n'+'node [shape="circle", style="filled", fillcolor="blue",fontcolor="black"];'+'\n')
 	for item in os.listdir(path):
 		p = os.path.join(path, item)
 		if os.path.isdir(p):
@@ -28,7 +28,7 @@ def t_dir2(path, prefix=u''):
 			print u'{}| |--(f){}'.format(prefix, item)
 
 file_1 = open ('file.dot','a')
-file_1.write('graph graphname {')
+file_1.write('graph graphname {node [shape="box", style="filled", fillcolor="green", fontcolor="black", fontsize="9"];'+'\n')
 
 
 t_dir2('/home/gorsing/script_afs_graf/test')
