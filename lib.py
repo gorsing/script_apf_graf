d1c203ef23f57c91840bd5a6275903ac30da9610
Gorsing <gorsing@meta.ua>
2015-03-19 23:40:57 +0200

    rev 4

---
+++ lib.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+import os
+
+def t_dir2(path, prefix=u''):
+	print u'{}(Dir){}'.format(prefix, os.path.basename(path))
+	file_1.write('"'+str(os.path.basename(path))+'";'+'\n')
+	for item in os.listdir(path):
+		p = os.path.join(path, item)
+		if os.path.isdir(p):
+			file_1.write('"'+str(os.path.basename(path))+'"--')
+			t_dir2(p, prefix + u'|   ')
+		else:
+			#file_1.write('"'+str(item)+'"'+';'+'\n')
+			print (str(os.path.join(path, item)))
+			print u'{}| |--(f){}'.format(prefix, item)
+
+file_1 = open ('file.dot','a')
+file_1.write('graph graphname {')
+
+
+
+t_dir2('/home/gorsing/script_afs_graf/test')
--- test/indir2/script.py
+++ test/indir2/script.py
@@ -14,26 +14,12 @@ def print_dir(path, prefix=u''):
 			print (str(os.path.join(path, item)))
 			print u'{}| |--(f){}'.format(prefix, item)
 
-def t_dir2(path, prefix=u''):
-	print u'{}(Dir){}'.format(prefix, os.path.basename(path))
-	file_1.write('"'+str(os.path.basename(path))+'";'+'\n')
-	for item in os.listdir(path):
-		p = os.path.join(path, item)
-		if os.path.isdir(p):
-			file_1.write('"'+str(os.path.basename(path))+'"--')
-			t_dir2(p, prefix + u'|   ')
-		else:
-			#file_1.write('"'+str(item)+'"'+';'+'\n')
-			print (str(os.path.join(path, item)))
-			print u'{}| |--(f){}'.format(prefix, item)
-
 file_1 = open ('file.dot','a')
 file_1.write('graph graphname {')
 
 
-t_dir2('/home/gorsing/script_afs_graf/test')
+
 print_dir('/home/gorsing/script_afs_graf/test')
-file_1.write('}')
 
 
 
